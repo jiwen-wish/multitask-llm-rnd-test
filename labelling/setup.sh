8032d84dbc0ad151df7cf6ee78a1b476fa85d920
Junhao Wang <junwang@contextlogic.com>
2023-01-04 05:08:43 +0000

    add dev env for data labelling on dgx1

---
+++ labelling/setup.sh
@@ -0,0 +1,6 @@
+# echo "# Add conda py38 path" >> ~/.bashrc 
+# echo "export PATH=\"/opt/conda/envs/py38/bin:$PATH\"" >> ~/.bashrc  # already added, thus skip
+source ~/.bashrc
+conda create -y --name py38 python=3.8.13
+/opt/conda/envs/py38/bin/python -m pip install -U -r requirements.txt
+/opt/conda/envs/py38/bin/python -m pip install prodigy -f https://$PRODIGY_KEY@download.prodi.gy
