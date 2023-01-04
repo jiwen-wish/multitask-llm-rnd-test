28fb5e31c8d2c20b9f08341deed85370d0b0d698
Junhao Wang <junwang@contextlogic.com>
2023-01-04 19:09:03 +0000

    fix dvc s3 dependency in dev container

--- datasets/setup.sh
+++ datasets/setup.sh
@@ -6,5 +6,6 @@ pip install -r requirements.txt
 git clone git@github.com:ContextLogic/pyhive_utilities.git $HOME/pyhive_utilities
 # install python deps for pyhive
 pip install -r $HOME/pyhive_utilities/requirements.txt
+pip install -U s3fs # fix dvc s3 dependency
 # install chromium and its driver for web scraping
 sudo apt-get update && sudo apt-get install -y chromium chromium-driver
