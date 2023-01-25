b2689bb0cc19716b27bb729f70b5ca53c70b7151
Junhao Wang <junwang@contextlogic.com>
2023-01-25 00:46:15 +0000

    manually build onnxruntime gpu dependency

--- coeus_inference/setup.sh
+++ coeus_inference/setup.sh
@@ -4,3 +4,6 @@ source ~/.bashrc
 conda create -y --name py38 python=3.8.13
 /opt/conda/envs/py38/bin/python -m pip install -U -r requirements.txt
 pip install -i https://pypi.infra.wish.com/simple/ coeus_model_registry_client[pytorch_1]
+pip install -U cmake
+git clone -b v1.13.1 git@github.com:microsoft/onnxruntime.git /tmp/onnxruntime
+cd /tmp/onnxruntime && ./build.sh --cuda_home /usr/local/cuda --cudnn_home /usr/lib/x86_64-linux-gnu/ --use_cuda
