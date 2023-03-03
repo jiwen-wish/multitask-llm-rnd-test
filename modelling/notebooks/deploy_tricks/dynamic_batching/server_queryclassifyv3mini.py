d32afdca84aaab79dffafe45ed9b18631f218612
Junhao Wang <junwang@contextlogic.com>
2023-03-03 16:24:57 +0000

    add batch size constraint for dynamic batching

--- modelling/notebooks/deploy_tricks/dynamic_batching/server_queryclassifyv3mini.py
+++ modelling/notebooks/deploy_tricks/dynamic_batching/server_queryclassifyv3mini.py
@@ -3,17 +3,15 @@ from starlette.responses import JSONResponse
 from starlette.routing import Route
 from transformers import pipeline
 import asyncio
-
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
-import torch
-import os
-import tempfile
-import importlib
-import sys
 from dvc.api import DVCFileSystem
+import tempfile
+import sys
+import os
+import importlib
 
 repo = 'git@github.com:ContextLogic/multitask-llm-rnd.git'
 path = '/modelling/notebooks/convert_pl_to_hf_ckpt/query_classify_v3_mini/hf_ckpt'
+max_batch_size = 100
 
 def import_path(path):
     module_name = os.path.basename(path).replace('-', '_')
@@ -26,17 +24,6 @@ def import_path(path):
     sys.modules[module_name] = module
     return module
 
-
-def load_pipeline():
-    """
-    pipe(['Classify query: apple'], batch_size=10)
-    """
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        fs = DVCFileSystem(repo, subrepos=True)
-        fs.get(path, tmpdirname, recursive=True)
-        pipe = import_path(f"{tmpdirname}/load_model.py").load_pipeline()
-    return pipe
-
 async def homepage(request):
     payload = await request.body()
     string = payload.decode("utf-8")
@@ -47,30 +34,25 @@ async def homepage(request):
 
 
 async def server_loop(q):
-    pipe = load_pipeline()
-
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        fs = DVCFileSystem(repo, subrepos=True)
+        fs.get(path, tmpdirname, recursive=True)
+        pipe = import_path(f"{tmpdirname}/load_model.py").load_pipeline()
     while True:
         strings = []
         queues = []
         (string, rq) = await q.get()
         strings.append(string)
         queues.append(rq)
-
-        take = 4
-        while not q.empty() and take > 0:
+        while not q.empty() and len(strings) < max_batch_size:
             (string, rq) = await q.get()
             strings.append(string)
             queues.append(rq)
-
-        if len(strings) == 0:
-            continue
 
         outs = pipe(strings, batch_size=len(strings))
         for (rq, out) in zip(queues, outs):
             await rq.put(out)
 
-        strings = []
-        queues = []
 
 app = Starlette(
     routes=[
