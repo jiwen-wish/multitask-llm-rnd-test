00a87eacad264365bbe9d71d8ab06b6837a27851
Junhao Wang <junwang@contextlogic.com>
2023-03-14 18:18:53 +0000

    download tokenizer

--- triton_inference/fetch_query_classify_v3.py
+++ triton_inference/fetch_query_classify_v3.py
@@ -1,9 +1,13 @@
 import s3fs
 import os
+from transformers import AutoTokenizer
 os.system('rm -rf tmp tmp_onnx')
 fs = s3fs.S3FileSystem()
 fs.get('structured-data-dev/coeus-gpu-multitask-ml/query-classify/v3/', 'tmp', recursive=True)
+tokenizer = AutoTokenizer.from_pretrained('tmp')
 os.system('optimum-cli export onnx --model=tmp --task=sequence-classification --device cuda tmp_onnx/')
 os.system('mkdir -p model_repository/query_classify_onnx/1')
 os.system('mv tmp_onnx/model.onnx model_repository/query_classify_onnx/1/')
+os.system('mkdir -p model_repository/query_classify_tokenizer/1')
+tokenizer.save_pretrained('model_repository/query_classify_tokenizer/1')
 os.system('rm -rf tmp tmp_onnx')
