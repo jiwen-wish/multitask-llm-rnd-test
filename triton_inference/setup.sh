6e4ed696841a86ad0f8576073ebb7a30c9dcb40c
Junhao Wang <junwang@contextlogic.com>
2023-03-14 20:31:36 +0000

    repro qcv3

--- triton_inference/setup.sh
+++ triton_inference/setup.sh
@@ -1,7 +1,10 @@
+# setup github 
+git config --global user.email "junwang@contextlogic.com"
+git config --global user.name "Junhao Wang"
 # install other deps
 pip3.8 install -r requirements.txt
 # download model
 python3.8 fetch_query_classify_v3.py
-rm -rf model_repository/query_classify_onnx
+rm -rf model_repository/query_classify_tokenizer
 # start server
 tritonserver --model-repositor model_repository/
