ece68636dfa573abca0883abb58d62002852c2dd
Junhao Wang <junwang@contextlogic.com>
2023-03-14 16:32:50 +0000

    fix model download and conversion

--- triton_inference/fetch_query_classify_v3.py
+++ triton_inference/fetch_query_classify_v3.py
@@ -1,7 +1,9 @@
 import s3fs
 import os
+os.system('rm -rf tmp tmp_onnx')
 fs = s3fs.S3FileSystem()
 fs.get('structured-data-dev/coeus-gpu-multitask-ml/query-classify/v3/', 'tmp', recursive=True)
-os.system('python3.8 -m optimum-cli export onnx --model=tmp --task=sequence-classification --optimize=O4 --device cuda tmp_onnx/')
+os.system('optimum-cli export onnx --model=tmp --task=sequence-classification --optimize O1 --device cuda tmp_onnx/')
 os.system('mkdir -p model_repository/query_classify_onnx/1')
 os.system('mv tmp_onnx/model.onnx model_repository/query_classify_onnx/1/')
+os.system('rm -rf tmp tmp_onnx')
