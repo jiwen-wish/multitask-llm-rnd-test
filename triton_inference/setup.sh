d20af00de2936cedc647ea2b598c719204be737a
Junhao Wang <junwang@contextlogic.com>
2023-03-14 19:00:23 +0000

    installl triton python server

--- triton_inference/setup.sh
+++ triton_inference/setup.sh
@@ -1,3 +1,12 @@
+# install triton python backend
+GIT_BRANCH_NAME=r22.09 && git clone https://github.com/triton-inference-server/python_backend -b $GIT_BRANCH_NAME
+cd python_backend
+mkdir build && cd build
+GIT_BRANCH_NAME=r22.09 && cmake -DTRITON_ENABLE_GPU=ON -DTRITON_BACKEND_REPO_TAG=$GIT_BRANCH_NAME -DTRITON_COMMON_REPO_TAG=$GIT_BRANCH_NAME -DTRITON_CORE_REPO_TAG=$GIT_BRANCH_NAME -DCMAKE_INSTALL_PREFIX:PATH=/opt/tritonserver
+make install
+# install other deps
 pip3.8 install -r requirements.txt
+# download model
 python3.8 fetch_query_classify_v3.py
+# start server
 tritonserver --model-repositor model_repository/
