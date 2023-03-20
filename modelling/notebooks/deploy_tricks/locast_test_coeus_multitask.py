f0e3a8c1f0c8eb9f1c45b3668df40e2e68a0728d
Junhao Wang <junwang@contextlogic.com>
2023-03-20 17:50:30 +0000

    test coeus stage

--- modelling/notebooks/deploy_tricks/locast_test_coeus_multitask.py
+++ modelling/notebooks/deploy_tricks/locast_test_coeus_multitask.py
@@ -3,13 +3,13 @@ import numpy as np
 
 class MyUser(HttpUser):
     wait_time = between(1, 2)
-    host = 'http://coeus-gpu-multitask-ml-dev.service.consul:8081'
+    host = 'http://coeus-gpu-multitask-ml-stage.service.consul:8081'
     
     @task
     def classify_query(self):
         q = (np.random.randint(1, 6) * 'dildo ').strip()
-        self.client.post("/", data=f"Classify query: {q}")
+        self.client.post("/query-classify/v3", json={"text": f"Classify query: {q}"})
 
 if __name__ == "__main__":
     import os
-    os.system("locust -f locast_test_coeus_multitask.py -u 80 -r 10 -t 2m")
+    os.system("locust --headless -f locast_test_coeus_multitask.py -u 40 -r 10 -t 2m")
