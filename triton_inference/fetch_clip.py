b3b6e321053010fd0a57913c3db5d55d101ad921
Junhao Wang <junwang@contextlogic.com>
2023-03-31 22:28:13 +0000

    fix embedding dimension

--- triton_inference/fetch_clip.py
+++ triton_inference/fetch_clip.py
@@ -1,11 +1,24 @@
 #%%
 import os
-from transformers import AutoConfig, AutoProcessor, CLIPModel
+from transformers import AutoConfig, AutoProcessor, CLIPModel, CLIPVisionModel
+from transformers.modeling_outputs import BaseModelOutputWithPooling
 from pathlib import Path
 from transformers.onnx import export
 from typing import Mapping, OrderedDict
 from transformers.onnx import OnnxConfig, validate_model_outputs
+from functools import partial
+import torch
 
+class CLIPImageEncoder(CLIPVisionModel):
+    def forward(self,
+        pixel_values: torch.FloatTensor
+    ):
+        outputs = self.vision_model(
+            pixel_values=pixel_values, return_dict=True
+        )
+        return BaseModelOutputWithPooling(
+            pooler_output=outputs.pooler_output.reshape(-1, 768)
+        )
 
 class EncoderOnnxConfig(OnnxConfig):
     @property
@@ -20,7 +33,7 @@ class EncoderOnnxConfig(OnnxConfig):
     def outputs(self) -> Mapping[str, Mapping[int, str]]:
         return OrderedDict(
             [
-                ("image_features", {0: "batch", 1: "dim"})
+                ("pooler_output", {0: "batch", 1: "dim"})
             ]
         )
 #%%
@@ -28,8 +41,7 @@ os.system('rm -rf tmp_onnx')
 
 config = AutoConfig.from_pretrained("openai/clip-vit-base-patch32")
 onnx_config = EncoderOnnxConfig(config)
-model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-model.forward = model.get_image_features
+model = CLIPImageEncoder.from_pretrained("openai/clip-vit-base-patch32")
 
 processor = AutoProcessor.from_pretrained("openai/clip-vit-base-patch32")
 #%%
@@ -37,10 +49,10 @@ os.system('mkdir -p tmp_onnx')
 onnx_path = Path("tmp_onnx/model.onnx")
 #%%
 onnx_inputs, onnx_outputs = export(processor.image_processor, model, onnx_config, onnx_config.default_onnx_opset, onnx_path)
-
-# validate_model_outputs(
-#     onnx_config, processor.image_processor, model, onnx_path, onnx_outputs, onnx_config.atol_for_validation
-# )
+#%%
+validate_model_outputs(
+    onnx_config, processor.image_processor, model, onnx_path, onnx_outputs, 1e-4
+)
 
 os.system('mkdir -p model_repository/clip_image_onnx/1')
 os.system('mv tmp_onnx/model.onnx model_repository/clip_image_onnx/1/')
