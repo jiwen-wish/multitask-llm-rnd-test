88bc905884817b93081e61b7fd96a024d5dd902a
junwang-wish <junwang@contextlogic.com>
2023-04-18 13:11:24 -0400

    Dev0417 (#11)
    * add 0417 pulled product attribution data
    
    * show some example
    
    * update data
    
    * update notebook
    
    * add first batch md data
    
    * start inference on sample
    
    * run inference on 11K stratified sample
    
    * parse oai response
    
    * first prod prelod batch

---
+++ datasets/data/2023q2tli_product_attribute_extraction/l2s.py
@@ -0,0 +1,43 @@
+commited_l2s = """Cellphones & Telecommunications > Mobile Phone Accessories
+Cellphones & Telecommunications > Phone Bags & Cases
+Home & Garden > Arts, Crafts & Sewing
+Home & Garden > Festive & Party Supplies
+Home & Garden > Garden Supplies
+Home & Garden > Home Decor
+Home & Garden > Home Textile
+Home & Garden > Kitchen,Dining & Bar
+Home & Garden > Pet Products
+Home Improvement > Lights & Lighting
+Jewelry & Accessories > Fine Jewelry
+Jewelry & Accessories > Necklaces & Pendants
+Jewelry & Accessories > Rings
+Shoes > Women's Shoes
+Sports > Fishing
+Sports > Sneakers
+Consumer Electronics > Earphones & Headphones
+Sports > Fitness & Body Building
+Sports > Hunting
+Sports > Camping & Hiking
+Home & Garden > Home Storage & Organization
+Home & Garden > Household Merchandise
+Luggage & Bags > Women's Bags
+Luggage & Bags > Men's Bags
+Shoes > Men's Shoes""".split('\n')
+at_risk_l2s = """Cellphones & Telecommunications > Cellphones
+Cellphones & Telecommunications > Refurbished Phones
+Cellphones & Telecommunications > iPhones
+Computer & Office > Storage Devices
+Computer & Office > Office Electronics
+Computer & Office > Tablets
+Computer & Office > Laptops
+Computer & Office > Computer Peripherals
+Computer & Office > Mouse & Keyboards
+Computer & Office > Desktops
+Computer & Office > Mini PC
+Consumer Electronics > Smart Electronics
+Consumer Electronics > Video Games
+Consumer Electronics > Home Audio & Video
+Security & Protection > Video Surveillance
+Home Improvement > Smart Home Systems""".split('\n')
+very_at_risk_l2s = """Beauty & Health > Makeup
+Beauty & Health > Beauty Essentials""".split('\n')
---
+++ datasets/data/2023q2tli_product_attribute_extraction/run_oai_inference.py
@@ -0,0 +1,45 @@
+import asyncio
+import aiohttp
+import logging
+import os 
+import json
+import pandas as pd
+from tqdm import tqdm
+
+OPENAI_KEY = os.environ['WISH_OPENAI_KEY_DEV']
+headers = {
+    "Content-Type": "application/json",
+    "Authorization": f"Bearer {OPENAI_KEY}"
+}
+
+async def call_oai(session, data):
+    try:
+        async with session.post(
+            'https://api.openai.com/v1/chat/completions',
+            headers=headers,
+            json=data
+        ) as response:
+            res = await response.json()
+        return res
+    except Exception as e:
+        logging.error(f"{data} failed due to {e}")
+        return None
+
+async def call_oais(datas):
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for data in datas:
+            task = asyncio.ensure_future(call_oai(session, data))
+            tasks.append(task)
+        reses = await asyncio.gather(*tasks)
+        return reses
+
+batch_size = 100
+# 30 min
+with open('product_attribute_extraction_2023q2tli_041723_validprompt_041823_sample_041823_oaiinfer_041823.json', 'w') as f:
+    for chunk in tqdm(pd.read_json('product_attribute_extraction_2023q2tli_041723_validprompt_041823_sample_041823.json', 
+            lines=True, chunksize=batch_size), total=int(11328/batch_size)):
+        res = asyncio.run(call_oais(chunk.prompt.tolist()))
+        chunk['oai_response'] = res 
+        for i in chunk.to_dict('records'):
+            f.write(json.dumps(i) + '\n')
